095d1abb725b2a40dbbc4c44cf6018242133b760
vb27 <vampblood87@gmail.com>
2020-03-18 20:35:54 -0700

    created and links javascript file. wrote comments to then work on at a later time.

---
+++ script.js
@@ -0,0 +1,6 @@
+// figure out how to get #currentDay ([Moment.js](https://momentjs.com/))
+// figure out how to get time and compare it to the rows time label
+    // change color based on the above info
+// save to local storage and never clear
+    // button.click("save")
+    // can give each row a variable to be saved to local storage so nothing is in an array or anything
